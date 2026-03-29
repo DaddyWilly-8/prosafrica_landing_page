35b21386030e983f25a3dae90fe5b7e49e5f47b1
EliyaDeus <preciouseliya1@gmail.com>
2026-03-29 10:41:47 +0300

    new landing page - first commit

--- components/VercelSpeedInsights.tsx
+++
@@ -1,7 +0,0 @@
-// Add this file to integrate Vercel Speed Insights with Next.js App Router
-'use client';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-
-export function VercelSpeedInsights() {
-  return <SpeedInsights />;
-}
--- lib/data/menuItems.ts
+++
@@ -1,7 +0,0 @@
-export const headerMenuItems = [
-  { label: "Home", href: "/", dropDown: false },
-  { label: "Industries", href: "/#industries", dropDown: false },
-  { label: "How It Works", href: "/#howItWorks", dropDown: false },
-  { label: "Company", href: "#", dropDown: false },
-  { label: "Contact Us", href: "/contact", dropDown: false },
-];
